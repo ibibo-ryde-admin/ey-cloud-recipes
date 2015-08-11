5c6976765a2c9a3dea3f58630475f4386735ed1a
Swapnil Jaiswal <swapnil.jaiswal@tradus.com>
2015-08-11 18:02:11 +0530

    uncommented recipes  mongodb, memcached from main/default.rb

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -58,7 +58,7 @@ include_recipe "memcached"
 
 #uncomment to run the exim::auth recipe
 #include_recipe "exim::auth"
-#include_recipe "mongodb"
+include_recipe "mongodb"
 
 #uncomment to run the resque recipe
 # include_recipe "resque"
